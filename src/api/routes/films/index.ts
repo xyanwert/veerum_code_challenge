95c630f7e81d6682d0de4acb6a8b4133212cf690
Francisco Berridi <francisco.berridi@fitmylife.come>
2019-07-03 18:10:23 -0600

    removed folder

--- src/api/routes/films/index.ts
+++
@@ -1,3 +0,0 @@
-import {Request, Response} from "express";
-const routes = require('express').Router();
-const search = require('./search');
--- src/api/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import {Request, Response} from "express";
-const routes = require('express').Router();
-
-routes.get('/', (req: Request, res: Response) => {
-    res.status(200).json({ message: 'Connected!' });
-});
-
-module.exports = routes;
